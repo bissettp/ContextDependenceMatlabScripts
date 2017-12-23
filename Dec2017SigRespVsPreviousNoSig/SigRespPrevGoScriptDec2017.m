bc5b7056d0aeb669e365376f56a414a4a04a56f4
Patrick Bissett <pbissett@stanford.edu>
2017-12-22 20:41:40 -0800

    SSDRequiredMakingMoreRobust

--- Dec2017SigRespVsPreviousNoSig/SigRespPrevGoScriptDec2017.m
+++ Dec2017SigRespVsPreviousNoSig/SigRespPrevGoScriptDec2017.m
@@ -3,9 +3,9 @@ clear all
 %Subject number in first column, block number in 2nd column, go RT in 3rd
 %column, SSD in 4th column, sig-resp RT in 5th column. Replace non-numbers in columns 3, 4, and 5 with a number
 %below the SSD min (I usually use -500). This kludge seems to work
-[SubjectSeq Block GoRTSeq SSDSeq SigRespRT] = textread('SequentialRTsSSDsTurkSimpleHigh.txt', '%f%f%f%f%f');
+[SubjectSeq Block GoRTSeq SSDSeq SigRespRT] = textread('SequentialRTsSSDsTurkMotorSelec.txt', '%f%f%f%f%f');
 
-SigRespCountCutoff = 3; %Threshold for the number of signal-respond trials at a specific SSD for that subject to be computed
+SigRespCountCutoff = 1; %Threshold for the number of signal-respond trials at a specific SSD for that subject to be computed
 MinimumSubjectsForAverage = 1; %Threshold for the number of subjects that pass the SigRespCountCutoff at that SSD for that SSD to be included in the group average
 
 %Could hardcode SSDMin or SSDMax if you only want to evaluate a subset of
@@ -17,6 +17,21 @@ NumberOfSSDs = size(unique(SSDSeq), 1)-1; %last -1 is to account for the -500 kl
 [SubjectNum] = textread('TurkN339.txt', '%f'); % can load in a subset of subject numbers here, as is necessary for our
 % Turk data
 SSDRequired = [150 200 250 300]; % use this to look only at subjects who pass the SigRespCountCutoff on these SSDs
+SSDIncrement = 50; %incremental difference between each SSD
+
+for t=SSDMin:SSDIncrement:SSDMax
+    v = 1; 
+    for u=SSDRequired:SSDIncrement:SSDMax
+        w = 1; 
+        if(u==t)
+            SSDRequiredIndices(w) = u; 
+        end
+        w = w + 1; 
+    end
+    v = v + 1;
+end
+        
+SSDRequiredIndices = [12 13 14 15]; % index of the SSD required in an array from SSDMin to SSDMax
 
 NoStopOutput = zeros(1, NumberOfSSDs, size(SubjectNum, 1));
 SigRespOutput = zeros(1, NumberOfSSDs, size(SubjectNum, 1));
@@ -24,7 +39,7 @@ SigRespOutput = zeros(1, NumberOfSSDs, size(SubjectNum, 1));
 for a=1:(size(SubjectNum, 1))
     SubjectNumber = SubjectNum(a);
     d = 1;
-    for c=SSDMin:50:SSDMax 
+    for c=SSDMin:SSDIncrement:SSDMax 
         e = 1; 
         for b=1:size(SubjectSeq)-1
             if(GoRTSeq(b) > 0 && SigRespRT(b+1) > 0 && SSDSeq(b+1) == c && SubjectSeq(b) == SubjectNumber)
@@ -44,7 +59,7 @@ SigRespOutput(SigRespOutput==0) = NaN;
 for f=1:(size(SubjectNum, 1))
     SubjectNumber2 = SubjectNum(f);
     h = 1; 
-    for g=SSDMin:50:SSDMax
+    for g=SSDMin:SSDIncrement:SSDMax
         if(isnan(NoStopOutput(SigRespCountCutoff, h, f))) == 0;
             SigRespScatterplotY(h, f) = (nanmean(SigRespOutput(:, h, f)));
             SigRespScatterplotX(h, f) = g;
@@ -84,9 +99,9 @@ end
 %MinimumSubjectsForAverage numbers of subjects at that SSD that had >=
 %SigrespCountCutoff number of signal-respond trials. 
 for l=1:NumberOfSSDs
-    FullSSDList(l, 1) = SSDMin+(l-1)*50;
+    FullSSDList(l, 1) = SSDMin+(l-1)*SSDIncrement;
     if sum(isnan(NoStopScatterplotY(l, :))) <= size(SubjectNum, 1) - MinimumSubjectsForAverage
-        OnlySSDsUsedForAverage(l, 1) = SSDMin+(l-1)*50; 
+        OnlySSDsUsedForAverage(l, 1) = SSDMin+(l-1)*SSDIncrement; 
         meanNoStopScatterplotY(l, 1) = nanmean(NoStopScatterplotY(l, :));
         meanSigRespScatterplotY(l, 1) = nanmean(SigRespScatterplotY(l, :));
         NoSigMinusSigResp = meanNoStopScatterplotY - meanSigRespScatterplotY;
@@ -127,8 +142,7 @@ for f=1:(size(SubjectNum, 1))
     end
 end
 
-%plot((nanmean(SigRespScatterplotXRequired(4:7, :), 2)), (nanmean(SigRespScatterplotYRequired(4:7, :), 2)), 'r', 'LineWidth', 4);
-plot((nanmean(SigRespScatterplotXRequired(4:7, :), 2)), ((nanmean(NoStopScatterplotYRequired(4:7, :), 2)) - (nanmean(SigRespScatterplotYRequired(4:7, :), 2))), 'r', 'LineWidth', 4);
+plot((nanmean(SigRespScatterplotXRequired(SSDRequiredIndices, :), 2)), ((nanmean(NoStopScatterplotYRequired(SSDRequiredIndices, :), 2)) - (nanmean(SigRespScatterplotYRequired(SSDRequiredIndices, :), 2))), 'r', 'LineWidth', 4);
 
 SortedSigRespOutput = sort(SigRespOutput, 1);
 SortedNoStopOutput = sort(NoStopOutput, 1); 
